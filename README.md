ab75f14169daee1b5c9e87d019184d31019520b3
moeinmir <moein.mirani@gmail.com>
2025-08-06 13:24:15 +0330

    test(): optimistic and pessimistic lock example added.

--- README.md
+++ README.md
@@ -1,26 +1,125 @@
-## Account Management:
-- View list of bank accounts
-- Add a new account
-- Edit account information
-- Delete an account
+# Optimistic And Pessimistic Lock Test 
 
-## Transaction Management:
-- View transaction history
-- Add a new transaction (deposit/withdrawal)
-- Check account balance
+### We have a account entity class that has a field annotated with @version: 
+- responsible for keeping the version and prevent synchronous data modification
 
-## User Management:
-- User registration and login
-- View their own accounts
+```angular2html
+    @Version
+    private int version;
 
-## Project Structure:
-- Web API section
+```
 
-## Technologies Used:
-- Java
-- Spring Boot
-- Spring MVC
-- Spring Data JPA
-- H2 Database
-- Maven (for dependency management)
+### In repository level the possibility of locking the access to the row is provided
 
+```angular2html
+
+@Lock(LockModeType.PESSIMISTIC_WRITE)
+@Query("SELECT a FROM Account a WHERE a.id = :id")
+Optional<Account> findByIdWithPessimisticLock(@Param("id") Long id);
+
+```
+
+### Here we test optimistic lock using java threads and synchronized and shared lock object
+
+```angular2html
+    @Test
+    public void testOptimisticLockPreventingSaveWhenItShould() throws InterruptedException {
+        CustomUser optUser = CustomUser.createNewUser("optuser", "optuser", "optuser", "password");
+        customUserRepository.save(optUser);
+        Account optAccount = Account.createNewAccountWithBalanceForTheTest(optUser);
+        accountRepository.save(optAccount);
+        Long optUserId = optAccount.getAccountInformation().getId();
+        Long optAccountId = optAccount.getAccountInformation().getId();
+        Object lock = new Object();
+        AtomicBoolean transactionInFirstThreadDone = new AtomicBoolean(false);
+        Thread firstThread = new Thread(() -> {
+            Account optAccountInFirstThread = accountRepository.findById(optAccountId).orElse(null);
+            synchronized (lock) {
+                Transaction transactionInFirstThread = optAccountInFirstThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+                transactionInFirstThreadDone.set(true);
+                lock.notify();
+                try {
+                    lock.wait();  // Wait for secondThread to finish its work
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    transactionRepository.save(transactionInFirstThread);
+                    accountRepository.save(optAccountInFirstThread);
+                } catch (Exception e) {
+                    System.out.println("the transaction in first thread could not be saved as we expected");
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        Thread secondThread = new Thread(() -> {
+            Account optAccountInSecondThread = accountRepository.findById(optAccountId).orElse(null);
+            synchronized (lock) {
+                while (!transactionInFirstThreadDone.get()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                Transaction transactionInSecondThread = optAccountInSecondThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+                transactionRepository.save(transactionInSecondThread);
+                accountRepository.save(optAccountInSecondThread);
+                System.out.println("transaction in second thread has been saved as we expected");
+                lock.notify();
+            }
+        });
+        firstThread.start();
+        secondThread.start();
+        firstThread.join();
+        secondThread.join();
+    }
+
+
+```
+
+### Here we test the pessimistic lock:
+
+```angular2html
+
+    @Test
+    public void testPessimisticLockWithWithExecutor() throws InterruptedException {
+        CustomUser optUser = CustomUser.createNewUser("optuser", "optuser", "optuser", "password");
+        customUserRepository.save(optUser);
+        Account optAccount = Account.createNewAccountWithBalanceForTheTest(optUser);
+        accountRepository.save(optAccount);
+        Long optUserId = optAccount.getAccountInformation().getId();
+        Long optAccountId = optAccount.getAccountInformation().getId();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Runnable firstTask = () -> {
+            System.out.println("account balance after two transaction");
+            Account optAccountInFirstThread = accountRepository.findByIdWithPessimisticLock(optAccountId).orElse(null);
+            Transaction transactionInFirstThread = optAccountInFirstThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+            transactionRepository.save(transactionInFirstThread);
+            accountRepository.save(optAccountInFirstThread);
+        };
+        Runnable secondTask = () -> {
+            System.out.println("account balance after two transaction");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Account optAccountInSecondThread = accountRepository.findByIdWithPessimisticLock(optAccountId).orElse(null);
+                if (optAccountInSecondThread == null) {
+                    throw new RuntimeException();
+                }
+            }
+            catch (Exception e) {
+                System.out.println("we could not access the account because first thread locked it");
+                System.out.println(e.getMessage());
+            }
+        };
+        executor.submit(firstTask);
+        executor.submit(secondTask);
+        executor.awaitTermination(5000, TimeUnit.MILLISECONDS);
+    }
+
+```
--- src/test/java/com/meb/account_management/AccountManagementApplicationTests.java
+++ src/test/java/com/meb/account_management/AccountManagementApplicationTests.java
@@ -15,11 +15,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
-
 import java.math.BigDecimal;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 
 @Profile("test")
 @SpringBootTest
@@ -36,7 +37,6 @@ class AccountManagementApplicationTests {
 
     @Autowired
     private TransactionRepository transactionRepository;
-
 
     @Test
     void contextLoads() {
@@ -64,8 +64,7 @@ class AccountManagementApplicationTests {
     }
 
     @Test
-    public void testTransferMoney(){
-
+    public void testTransferMoney() {
         CustomUser userWithMoney = CustomUser.createNewUser("userwithmoney", "userwithmoney", "userwithmoney", "password");
         customUserRepository.save(userWithMoney);
         Account accountWithMoney = Account.createNewAccountWithBalanceForTheTest(userWithMoney);
@@ -83,13 +82,13 @@ class AccountManagementApplicationTests {
         String firstRequesterUsername = userWithMoney.getUserInformation().getUsername();
         ServiceResponse<TransferMoneyResponseDto> transferMoneyResponse = accountService.transferMoney(transferFromFirstRequestDto, firstRequesterUsername);
 
-        if (transferMoneyResponse.isSuccess()){
+        if (transferMoneyResponse.isSuccess()) {
             System.out.println(transferMoneyResponse.getResult().getSourceAccountRemainingBalance());
         }
     }
 
     @Test
-    public void testOptimisticLock() throws InterruptedException {
+    public void testOptimisticLockForTransfer() throws InterruptedException {
         CustomUser userWithMoney = CustomUser.createNewUser("userwithmoney", "userwithmoney", "userwithmoney", "password");
         customUserRepository.save(userWithMoney);
         Account firstAccountWithMoney = Account.createNewAccountWithBalanceForTheTest(userWithMoney);
@@ -106,8 +105,6 @@ class AccountManagementApplicationTests {
         int NUMBER_OF_TRIALS_OF_SECOND_ACCOUNT_WITH_MONEY = 500;
         AtomicInteger numberOfSuccessFullTransfersFromFirstAccountWithMoney = new AtomicInteger();
         AtomicInteger numberOfSuccessFullTransfersFromSecondAccountWithMoney = new AtomicInteger();
-
-
         Runnable transferFromFirst = () -> {
             TransferMoneyRequestDto transferFromFirstRequestDto = TransferMoneyRequestDto.builder()
                     .sourceAccountId(firstAccountWithMoney.getAccountInformation().getId())
@@ -116,12 +113,11 @@ class AccountManagementApplicationTests {
             String firstRequesterUsername = userWithMoney.getUserInformation().getUsername();
             for (int i = 0; i < NUMBER_OF_TRIALS_OF_FIRST_ACCOUNT_WITH_MONEY; i++) {
                 ServiceResponse<TransferMoneyResponseDto> transferMoneyResponse = accountService.transferMoney(transferFromFirstRequestDto, firstRequesterUsername);
-            	if (transferMoneyResponse.isSuccess()){
+                if (transferMoneyResponse.isSuccess()) {
                     numberOfSuccessFullTransfersFromFirstAccountWithMoney.addAndGet(1);
                 }
             }
         };
-
         Runnable transferFromSecond = () -> {
             String secondRequesterUsername = userWithMoney.getUserInformation().getUsername();
             TransferMoneyRequestDto transferFromSecondRequestDto = TransferMoneyRequestDto.builder()
@@ -130,7 +126,7 @@ class AccountManagementApplicationTests {
                     .amount(BigDecimal.ONE).build();
             for (int i = 0; i < NUMBER_OF_TRIALS_OF_SECOND_ACCOUNT_WITH_MONEY; i++) {
                 ServiceResponse<TransferMoneyResponseDto> transferMoneyResponse = accountService.transferMoney(transferFromSecondRequestDto, secondRequesterUsername);
-                if(transferMoneyResponse.isSuccess()){
+                if (transferMoneyResponse.isSuccess()) {
                     numberOfSuccessFullTransfersFromSecondAccountWithMoney.addAndGet(1);
 
                 }
@@ -146,6 +142,155 @@ class AccountManagementApplicationTests {
         System.out.println(numberOfSuccessFullTransfersFromFirstAccountWithMoney);
         System.out.println(numberOfSuccessFullTransfersFromSecondAccountWithMoney);
     }
+
+    @Test
+    public void testOptimisticLockPreventingSaveWhenItShould() throws InterruptedException {
+        CustomUser optUser = CustomUser.createNewUser("optuser", "optuser", "optuser", "password");
+        customUserRepository.save(optUser);
+        Account optAccount = Account.createNewAccountWithBalanceForTheTest(optUser);
+        accountRepository.save(optAccount);
+        Long optUserId = optAccount.getAccountInformation().getId();
+        Long optAccountId = optAccount.getAccountInformation().getId();
+        Object lock = new Object();
+        AtomicBoolean transactionInFirstThreadDone = new AtomicBoolean(false);
+        Thread firstThread = new Thread(() -> {
+            Account optAccountInFirstThread = accountRepository.findById(optAccountId).orElse(null);
+            synchronized (lock) {
+                Transaction transactionInFirstThread = optAccountInFirstThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+                transactionInFirstThreadDone.set(true);
+                lock.notify();
+                try {
+                    lock.wait();  // Wait for secondThread to finish its work
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    transactionRepository.save(transactionInFirstThread);
+                    accountRepository.save(optAccountInFirstThread);
+                } catch (Exception e) {
+                    System.out.println("the transaction in first thread could not be saved as we expected");
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        Thread secondThread = new Thread(() -> {
+            Account optAccountInSecondThread = accountRepository.findById(optAccountId).orElse(null);
+            synchronized (lock) {
+                while (!transactionInFirstThreadDone.get()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                Transaction transactionInSecondThread = optAccountInSecondThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+                transactionRepository.save(transactionInSecondThread);
+                accountRepository.save(optAccountInSecondThread);
+                System.out.println("transaction in second thread has been saved as we expected");
+                lock.notify();
+            }
+        });
+        firstThread.start();
+        secondThread.start();
+        firstThread.join();
+        secondThread.join();
+    }
+
+
+    @Test
+    public void testOptimisticLockNotPreventingSaveWhenItShould() throws InterruptedException {
+        CustomUser optUser = CustomUser.createNewUser("optuser", "optuser", "optuser", "password");
+        customUserRepository.save(optUser);
+        Account optAccount = Account.createNewAccountWithBalanceForTheTest(optUser);
+        accountRepository.save(optAccount);
+        Long optUserId = optAccount.getAccountInformation().getId();
+        Long optAccountId = optAccount.getAccountInformation().getId();
+        Object lock = new Object();
+        AtomicBoolean transactionInFirstThreadDone = new AtomicBoolean(false);
+        AtomicBoolean transactionInSecondThreadDone = new AtomicBoolean(false);
+        Thread firstThread = new Thread(() -> {
+            Account optAccountInFirstThread = accountRepository.findById(optAccountId).orElse(null);
+            synchronized (lock) {
+                Transaction transactionInFirstThread = optAccountInFirstThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+                transactionRepository.save(transactionInFirstThread);
+                accountRepository.save(optAccountInFirstThread);
+                System.out.println("transaction in first thread has been saved as we expected");
+                transactionInFirstThreadDone.set(true);
+                lock.notify();
+            }
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Account optAccountAfterTransactionInSecondThread = accountRepository.findById(optAccountId).orElse(null);
+            System.out.println("account balance after two transaction");
+            System.out.println(optAccountAfterTransactionInSecondThread.getAccountInformation().getBalance());
+        });
+        Thread secondThread = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Account optAccountInSecondThread = accountRepository.findById(optAccountId).orElse(null);
+            synchronized (lock) {
+                Transaction transactionInSecondThread = optAccountInSecondThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+                transactionRepository.save(transactionInSecondThread);
+                accountRepository.save(optAccountInSecondThread);
+                System.out.println("transaction in second thread has been saved as we expected");
+                transactionInSecondThreadDone.set(true);
+                lock.notify();
+            }
+        });
+        firstThread.start();
+        secondThread.start();
+        firstThread.join();
+        secondThread.join();
+    }
+
+    @Test
+    public void testPessimisticLockWithWithExecutor() throws InterruptedException {
+        CustomUser optUser = CustomUser.createNewUser("optuser", "optuser", "optuser", "password");
+        customUserRepository.save(optUser);
+        Account optAccount = Account.createNewAccountWithBalanceForTheTest(optUser);
+        accountRepository.save(optAccount);
+        Long optUserId = optAccount.getAccountInformation().getId();
+        Long optAccountId = optAccount.getAccountInformation().getId();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Runnable firstTask = () -> {
+            System.out.println("account balance after two transaction");
+            Account optAccountInFirstThread = accountRepository.findByIdWithPessimisticLock(optAccountId).orElse(null);
+            Transaction transactionInFirstThread = optAccountInFirstThread.addTransaction(BigDecimal.ONE, TransactionType.DEBIT, optUserId);
+            transactionRepository.save(transactionInFirstThread);
+            accountRepository.save(optAccountInFirstThread);
+        };
+        Runnable secondTask = () -> {
+            System.out.println("account balance after two transaction");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Account optAccountInSecondThread = accountRepository.findByIdWithPessimisticLock(optAccountId).orElse(null);
+                if (optAccountInSecondThread == null) {
+                    throw new RuntimeException();
+                }
+            }
+            catch (Exception e) {
+                System.out.println("we could not access the account because first thread locked it");
+                System.out.println(e.getMessage());
+            }
+        };
+        executor.submit(firstTask);
+        executor.submit(secondTask);
+        executor.awaitTermination(5000, TimeUnit.MILLISECONDS);
+    }
+
+
+
 }
 
 
